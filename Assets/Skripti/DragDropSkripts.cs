e027e68e939a7ba6437c6e65d6d6c511733541eb
Pauls <gustavspauls1@gmail.com>
2022-03-08 14:18:45 +0200

    Uzsakta skriptu rakstisana

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Jaimporte, vai lietotu visus I interfesus
+using UnityEngine.EventSystems;
+
+public class DragDropSkripts : MonoBehaviour,IPointerDownHandler,IBeginDragHandler,IDragHandler,IEndDragHandler {
+	//Velkamajam aobjektam piestiprina konponentu
+	private CanvasGroup kanvasGrupa;
+	//Prieks parvietojama objekta atrasanas vietas uzglabasanos
+	private RectTransform velkObjRectTransf;
+	//Norda uz objektu skriptu
+	public Objekti objektuSkripts;
+	// Use this for initialization
+	void Start () {
+		//Pieklust ibjekta piestripinajusi CanvasGroup komponente
+		kanvasGrupa = GetComponent<CanvasGroup>();
+		//Pisklust objekta recttransform komenentu
+		velkObjRectTransf = GetComponent<RectTransform>();
+		
+	}
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Uzklikskinats uz velkama objekta");
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsakta vilksana");
+		//Vwelkot objektu tas palike caurspidigs
+		kanvasGrupa.alpha = 0.6f;
+		//Lai objektu velkot iet carui rajcasts start
+		kanvasGrupa.blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		Debug.Log ("Notiek vilksana");
+		//Maina objekta x,y koodrinates
+		velkObjRectTransf.anchoredPosition+=notikums.delta / objektuSkripts.kanva.scaleFactor;
+		//Lai objektu velkot iet carui rajcasts start
+		kanvasGrupa.blocksRaycasts = false;
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Beigta objekta vilksana");
+		//Vwl turpinasim
+	}
+}
---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objekti : MonoBehaviour {
+	//Uzglaba ainava esoso kanvu
+	public Canvas kanva;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
